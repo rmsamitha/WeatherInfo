a939012da37e88be3ea4591724e89e3bc591d28c
rmsamitha <rmschathuranga@gmail.com>
2025-04-14 20:28:21 +1200

    Refactoring code

--- WeatherInfoService/src/main/java/com/sam/weather/impl/WeatherImplUtils.java
+++ WeatherInfoService/src/main/java/com/sam/weather/impl/WeatherImplUtils.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2025 Samitha Chathuranga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sam.weather.impl;
 
 import com.sam.weather.impl.dto.ErrorDTO;
-import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -11,6 +26,9 @@ import javax.ws.rs.core.Response;
 public class WeatherImplUtils {
     private static CloseableHttpClient httpClient;
 
+    /**
+     * Sets the HTTP client with custom configurations.
+     */
     public static void setHttpClient() {
         httpClient = HttpClients.custom()
                 .setMaxConnTotal(100) // Set maximum total connections
@@ -18,6 +36,11 @@ public class WeatherImplUtils {
                 .build();
     }
 
+    /**
+     * Returns the HTTP client.
+     *
+     * @return CloseableHttpClient instance
+     */
     public static CloseableHttpClient getHttpClient() {
         if (httpClient == null) {
             setHttpClient();
--- WeatherInfoService/src/main/java/com/sam/weather/impl/dto/ErrorDTO.java
+++ WeatherInfoService/src/main/java/com/sam/weather/impl/dto/ErrorDTO.java
@@ -1,28 +1,40 @@
+/*
+ * Copyright 2025 Samitha Chathuranga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sam.weather.impl.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.*;
+
 import javax.validation.constraints.NotNull;
 
+/**
+ * ErrorDTO class represents an error response in the API.
+ * It contains details such as error code, message, description, and more information.
+ */
 @ApiModel(description = "")
 public class ErrorDTO {
-
-
     @NotNull
     private Long code = null;
 
     @NotNull
     private String message = null;
-
-
     private String description = null;
-
-
     private String moreInfo = null;
 
-
-    /**
-     **/
     @ApiModelProperty(required = true, value = "")
     @JsonProperty("code")
     public Long getCode() {
@@ -32,7 +44,6 @@ public class ErrorDTO {
     public void setCode(Long code) {
         this.code = code;
     }
-
 
     /**
      * Error message.
@@ -47,7 +58,6 @@ public class ErrorDTO {
         this.message = message;
     }
 
-
     /**
      * A detail description about the error message.
      */
@@ -61,7 +71,6 @@ public class ErrorDTO {
         this.description = description;
     }
 
-
     /**
      * Preferably an url with more details about the error.
      */
@@ -74,11 +83,6 @@ public class ErrorDTO {
     public void setMoreInfo(String moreInfo) {
         this.moreInfo = moreInfo;
     }
-
-
-
-
-
 
     @Override
     public String toString() {
